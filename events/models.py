93538650d12567074c2de8e5ad4afd781366bbc5
heyylateef <lateef.lotz@gmail.com>
2021-03-31 22:27:27 -0400

    Added eventName to model
    - Edited models.py to include eventName

--- events/models.py
+++ events/models.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 # Create your models here.
 
 class Event(models.Model):
+    eventName = models.CharField(u'Name of event', help_text=u'Name of event', max_length=300, )
     day = models.DateField(u'Day of the event', help_text=u'Day of the event')
     start_time = models.TimeField(u'Starting time', help_text=u'Starting time')
     end_time = models.TimeField(u'Final time', help_text=u'Final time')
@@ -30,7 +30,7 @@ class Event(models.Model):
 
     def get_absolute_url(self):
         url = reverse('admin:%s_%s_change' % (self._meta.app_label, self._meta.model_name), args=[self.id])
-        return u'<a href="%s">%s</a>' % (url, str(self.start_time))
+        return u'<a href="%s">%s</a>' % (url, str(self.eventName))
 
     def clean(self):
         if self.end_time <= self.start_time:
